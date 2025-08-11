91a78949b880f169fe6aaff022acbb04217b01b8
Joshua Cadavez <joshua.cadavez@outlook.com>
2025-08-11 15:59:14 -0400

    1 - Installed Pydantic to setup the `image` data model

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Image(BaseModel):
+    uri: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/process-image")
+async def process_image(image: Image):
+    return image
+    # return {"message": f"Processing image at {image.uri}"}
